f46f52759350b5dcb06007fc52408b32cd33df7e
MemoRodriguez28 <memorodriguez2809@outlook.com>
2023-04-19 10:32:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from lexico import *
-print("hello world")
-test()
+
+fuente = "PRINT a" #Longitud es 7
+lexico = Lexico(fuente)
+
+#while len(fuente) != None:
+#    lexico.siguiente()
+
+while lexico.asomar() != '\0':
+    print(lexico.carActual)
+    lexico.siguiente()
