0de1d351a3c22ee120e305f8940f462c52e22192
MemoRodriguez28 <memorodriguez2809@outlook.com>
2023-04-26 20:52:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from lexico import *
 
-codigo = "<= == + * != - / >= #Comentario\n 12.3 \"pepe\""
+#codigo = "<= == + * != - / >= #Comentario\n 12.3 \"pepe\" IF var = 0 "
+codigo = "LET var = 1\n var = var + 2\n PRINT \"Var: \" + var "
 
 lexico = Lexico(codigo)
 token = lexico.getToken()
