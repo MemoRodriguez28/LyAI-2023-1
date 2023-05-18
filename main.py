002e7526f0e813e385ddb50b4d4a9cc653f39960
MemoRodriguez28 <memorodriguez2809@outlook.com>
2023-05-17 20:56:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 from lexico import *
+from sintatico import *
+import sys
+
+def main():
+    print("CompiladorCito")
+    
+    if len(sys.argv) != 2:
+        sys.exit("Error: Se necesita un archivo fuente para poder compilar.")
+    with open(sys.argv[1], 'r') as archivo:
+        fuente = archivo.read()
+
+
+    lexico = Lexico(fuente)
+    sintatico = Sintatico(lexico)
+    
+    sintatico.programa() #Empezar el analizador sintatico
+    print("Analisis completo.")
+
+
 
 #codigo = "<= == + * != - / >= #Comentario\n 12.3 \"pepe\" IF var = 0 "
-codigo = "LET var = 1\n var = var + 2\n PRINT \"Var: \" + var "
-
-lexico = Lexico(codigo)
-token = lexico.getToken()
+#codigo = "LET var = 1\n var = var + 2\n PRINT \"Var: \" + var "
+#token = lexico.getToken()
 #Siempre y cuando no sea EOF, continuar leyendo tokens
-while token.token != TipoToken.EOF:
-    print(token.token)
-    token = lexico.getToken()
+#while token.token != TipoToken.EOF:
+    #print(token.token)
+    #token = lexico.getToken()
     
