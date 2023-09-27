d71744d74d6feadc7cfa0f012ace5439522df6a7
MemoRodriguez28 <memorodriguez2809@outlook.com>
2023-09-26 17:45:27 -0700

    Update TAC.py

--- TAC.py
+++ TAC.py
@@ -1,7 +1,11 @@
 from RPN import RPN
 
-#infija = "x=(a+b)*c-(d+e)"
-#postfija = RPN(infija)
+infija = "x=(a+b)*c-(d+e)"
+postfija = RPN(infija)
+
 def TAC(postfija):
-    
+    operadores = ['+', '-', '*', '/', '=']
+    stack = []
+    tac = []
+    for i in postfija:
     return TAC
