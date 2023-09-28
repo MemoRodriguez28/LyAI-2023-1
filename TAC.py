2c99173a84232492e04e0a74b58467858cbcfc73
MemoRodriguez28 <memorodriguez2809@outlook.com>
2023-09-27 17:33:21 -0700

    Update TAC.py

--- TAC.py
+++ TAC.py
@@ -7,5 +7,21 @@ def TAC(postfija):
     operadores = ['+', '-', '*', '/', '=']
     stack = []
     tac = []
+    cont = 1
+    
     for i in postfija:
-    return TAC
+        if i not in operadores:
+            stack.append(i)
+        else:
+            op2 = stack.pop()
+            op1 = stack.pop()
+            temp = "t" + str(cont)
+            cont += 1
+            if i == '=':
+                tac.append(op1 + ":= " + op2)
+            else:
+                tac.append(temp + " := " + op1 + i + op2)
+                stack.append(temp)
+    return tac
+
+print(TAC(postfija))
